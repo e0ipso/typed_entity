ccc4d14395da289c13f84393b20dafffdca0b50f
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-02-25 08:20:22 +0100

    Issue #1: Minor improvements and syntax fix.

--- src/TypedEntity/TypedEntityManager.php
+++ src/TypedEntity/TypedEntityManager.php
@@ -13,36 +13,37 @@ class TypedEntityManager implements TypedEntityManagerInterface {
    * {@inheritdoc}
    */
   public static function create($entity_type, $entity) {
-    $class = static::getClass($entity_type, $entity);
-    new $class_name($entity_type, $entity);
+    $class_name = static::getClass($entity_type, $entity);
+    return new $class_name($entity_type, $entity);
   }
-  
+
   /**
    * Helper function to get a class name given an entity type and an entity.
-   * 
+   *
    * @param string $entity_type
    *   The entity type
    * @param object $entity
    *   The entity object.
-   * 
-   * @return string|bool
-   *   A valid class name if one exists, or FALSE if a class could not be found.
+   *
+   * @return string
+   *   A valid class name if one exists. It defaults to TypedEntity.
    */
-  public static getClass($entity_type, $entity) {
-    $classes = &drupal_static(__METHOD__);
-    
+  public static function getClass($entity_type, $entity) {
+    $classes = &drupal_static(__CLASS__ . '::' . __METHOD__);
+
     if (!isset($classes)) {
       $classes = array();
       if ($cache = cache_get('typed_entity_classes', 'cache_bootstrap')) {
         $classes = $cache->data;
       }
     }
-    
+
     list( , , $bundle) = entity_extract_ids($entity_type, $entity);
     $cid = $entity_type . ':' . $bundle;
     if (!isset($classes[$cid])) {
-      // The default class should always be TypedEntity.
-      $classes[$cid] = 'TypedEntity';
+      // The default class should always be TypedEntity. Assume that TypedEntity
+      // is under the same namespace as TypedEntityManager.
+      $classes[$cid] = __NAMESPACE__ . '\\TypedEntity';
       $candidates = static::getClassNameCandidates($entity_type, $bundle);
       foreach ($candidates as $candidate) {
         if (class_exists($candidate)) {
@@ -52,7 +53,7 @@ class TypedEntityManager implements TypedEntityManagerInterface {
       }
       cache_set('typed_entity_classes', $classes, 'cache_bootstrap');
     }
-    
+
     return $classes[$cid];
   }
 
@@ -74,12 +75,16 @@ class TypedEntityManager implements TypedEntityManagerInterface {
    */
   protected static function getClassNameCandidates($entity_type, $bundle) {
     $names = array();
-    $class_name_entity = 'Typed' . static::camelize($entity_type);
-    $class_name_bundle = 'Typed' . static::camelize($entity_type) . static::camelize($bundle);
+    $class_names = array();
+    if (!empty($bundle)) {
+      $class_names[] = 'Typed' . static::camelize($entity_type) . static::camelize($bundle);
+    }
+    // It is important to add the most specific first.
+    $class_names[] = 'Typed' . static::camelize($entity_type);
     foreach (module_list() as $module_name) {
-      // It is important to add the most specific first.
-      $names[] = '\\Drupal\\' . $module_name . '\\TypedEntity\\' . $class_name_bundle;
-      $names[] = '\\Drupal\\' . $module_name . '\\TypedEntity\\' . $class_name_entity;
+      foreach ($class_names as $class_name) {
+        $names[] = '\\Drupal\\' . $module_name . '\\TypedEntity\\' . $class_name;
+      }
     }
     return $names;
   }
